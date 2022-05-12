2efa67dc8e7cc3bd7d472ffc0f8b9bcd274a5f6c
oThommy <tvdhil@datho.com>
2022-05-12 08:31:12 +0200

    sys.argv[0] might be a bug

--- src/solution.py
+++ src/solution.py
@@ -12,7 +12,7 @@ class Solution:
     E: dict[NodeId, dict[NodeId, bool]]
     ilp: Ilp
     __basename: str = field(
-        init=False, 
+        init=False,  #https://stackoverflow.com/questions/4152963/get-name-of-current-script-in-python#:~:text=You%20can%20use%20__file,path.
         default=os.path.basename(sys.argv[0]) # returns script name that was invoked from the command line (instead of this module)
     )
     # date
